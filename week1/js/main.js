1614cd845b2f9bee2a3e421e74ff5aa8bc63a9b5
Mark McAninch <awacs68@fullsail.udu>
2013-03-07 22:50:46 -0500

    Update to the repo

--- week1/js/main.js
+++ week1/js/main.js
@@ -27,3 +27,35 @@ $('#form').on('pageinit', function(){
   alert("Information Saved!");
  };       
 });
+
+function getData(){
+    if(localStorage.length === 0){
+           alert("There is no data in Local Storage so default data was added.");
+           autoFillData();
+  }
+  var makeDiv = document.createElement("form");
+  makeDiv.setAttribute("id", "items");
+  var makeList = document.createElement("ul");
+  makeDiv.appendChild(makeList);
+  document.body.appendChild(makeDiv);
+  $("items").style.display = "block"
+  for(var i=0, len=localStorage.length; i<len;i++){
+    var makeli = document.createElement("li");
+    var linksLi = document.createElement("li")
+    makeList.appendChild(makeli);
+    var key = localStorage.key(i);
+    var value = localStorage.getItem(key);
+    var obj = JSON.parse(value);
+    var makeSubList = document.createElement("ul");
+    makeli.appendChild(makeSubList);
+    getImage(obj.vehicle[1], makeSubList);
+    for(var n in obj){
+      var makeSubli = document.createElement("li");
+      makeSubList.appendChild(makeSubli);
+      var optSubText = obj[n][0]+" "+obj[n][1];
+      makeSubli.innerHTML = optSubText;
+      makeSubList.appendChild(linksLi);
+    }
+    makeItemLinks(localStorage.key(i), linksLi);
+    }
+  }
