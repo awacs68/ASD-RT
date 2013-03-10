56ddb771ee494dd773470803512fee1719495769
Mark McAninch <awacs68@fullsail.udu>
2013-03-10 09:47:00 -0400

    Updated the repo

--- week1/js/main.js
+++ week1/js/main.js
@@ -2,6 +2,8 @@
 // ASD //
 // Term: 1303 //
 
+// Save Function //
+
 $('#form').on('pageinit', function(){
    
     var myForm = $('#ownerform');
@@ -28,34 +30,59 @@ $('#form').on('pageinit', function(){
  };       
 });
 
-function getData(){
-    if(localStorage.length === 0){
-           alert("There is no data in Local Storage so default data was added.");
-           autoFillData();
-  }
-  var makeDiv = document.createElement("form");
-  makeDiv.setAttribute("id", "items");
-  var makeList = document.createElement("ul");
-  makeDiv.appendChild(makeList);
-  document.body.appendChild(makeDiv);
-  $("items").style.display = "block"
-  for(var i=0, len=localStorage.length; i<len;i++){
-    var makeli = document.createElement("li");
-    var linksLi = document.createElement("li")
-    makeList.appendChild(makeli);
-    var key = localStorage.key(i);
-    var value = localStorage.getItem(key);
-    var obj = JSON.parse(value);
-    var makeSubList = document.createElement("ul");
-    makeli.appendChild(makeSubList);
-    getImage(obj.vehicle[1], makeSubList);
-    for(var n in obj){
-      var makeSubli = document.createElement("li");
-      makeSubList.appendChild(makeSubli);
-      var optSubText = obj[n][0]+" "+obj[n][1];
-      makeSubli.innerHTML = optSubText;
-      makeSubList.appendChild(linksLi);
-    }
-    makeItemLinks(localStorage.key(i), linksLi);
-    }
-  }
+$('#loaddatapage').on('pageinit', function(){ 
+
+// Json Function //
+
+ $("#JSON").on('click', function(){
+$("#dataloading").empty();
+
+  $.ajax({
+    url: 'xhr/data.json',
+    type: 'GET',
+    dataType: 'json',
+    success: function(json){
+           console.log(json);
+             for(var i=0, j= json.contacts.length; i<j; i++){
+              var myJson = json.contacts[i];
+              $(''+
+                '<li>'+ myJson.fname +'</li>'+
+                '<li>'+ myJson.lname +'</li>'+
+                '<li>'+ myJson.year +'</li>'+
+                '<li>'+ myJson.make +'</li>'+
+                '<li>'+ myJson.model +'</li>'+
+                '<li>'+ myJson.repairs +'</li>'
+                ).appendTo('#dataloading');
+            };
+    }   
+     });   
+        }); 
+$("#XML").on('click', function(){
+  $("#dataloading").empty();
+  $.ajax({
+    url: 'xhr/data.xml',
+    type: 'GET',
+    dataType: 'xml',
+    success: function(xml){
+           console.log(xml);
+            $(xml).find('information').each(function(){
+              var myXml = {};
+              myXml.fname = $(this).find('fname').text();
+              myXml.lname = $(this).find('lname').text();
+              myXml.year = $(this).find('year').text();
+              myXml.make = $(this).find('make').text();
+              myXml.model = $(this).find('model').text();
+              myXml.repairs = $(this).find('repairs').text();
+              $(''+
+                '<li>'+ myXml.fname +'</li>'+
+                '<li>'+ myXml.lname +'</li>'+
+                '<li>'+ myXml.year +'</li>'+
+                '<li>'+ myXml.make +'</li>'+
+                '<li>'+ myXml.model +'</li>'+
+                '<li>'+ myXml.repairs +'</li>'
+                ).appendTo('#dataloading');
+            });
+    }   
+     });   
+}); 
+   });
